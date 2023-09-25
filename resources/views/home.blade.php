0e30fd818ff01300695a4114ea9312ce1bcf4808
Abeer Abu Mosameh <rog.devloper9@gmail.com>
2023-09-25 15:05:39 +0300

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,7 +26,14 @@
                     <div class="row justify-content-center pt-8 px-8 pt-md-50 px-md-0">
                         <div class="col-md-9">
                             <div class="rounded-xl overflow-hidden w-300 max-h-md-450px mb-30">
-                                <img src="{{asset('admin/assets/media/bg/welcome2.png')}}" class="w-100" alt="">
+                                @if(\Illuminate\Support\Facades\Auth::user()->level == 1)
+                                    <img src="{{asset('admin/assets/media/bg/welcome.png')}}" class="w-100" alt="">
+                                @elseif(\Illuminate\Support\Facades\Auth::user()->level == 2)
+                                    <img src="{{asset('admin/assets/media/bg/welcome2.png')}}" class="w-100" alt="">
+                                @elseif(\Illuminate\Support\Facades\Auth::user()->level == 3)
+                                    <img src="{{asset('admin/assets/media/bg/welcome3.png')}}" class="w-100" alt="">
+
+                                @endif
                             </div>
 
                         </div>
